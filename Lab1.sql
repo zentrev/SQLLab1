c1602c11b6919a4410debb942322eeadcaa3ab90
Neondangel <jose.rosado853@gmail.com>
2018-04-29 17:52:29 -0600

    Modified 6.1

--- Lab1.sql
+++ Lab1.sql
@@ -158,7 +158,6 @@ Create Table AllProducts (
 	go
 
 
-
 select *
 From AllProducts
 Where SourceTable = 'Gasoline'
@@ -271,15 +270,15 @@ the constraint?
 Return the top 100 products that had the largest month-to-month change in price as 
 a percent (p2 - p1)/p1.
 */
+use AP
 
 select *
+from AllProducts
+
+select TOP 100 *, ((CAST(p2.value as float) - CAST(p1.value as float)) / CAST(p1.value as float)) * 100 as 'percent'
 from AllProducts p1 join AllProducts p2
 on p1.Date = p2.Date + 1
-
-
-
-
-
+where p1.series_id = p2.series_id
 
 /* 6.2 ENGLISH
 How did you handle division by 0?  Are the zeros good data, or bad?  Justify your decision.
